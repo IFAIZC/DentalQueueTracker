cd6a93a3c1a6afd74efa125ec324391443b449ee
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-03-27 12:03:59 +0800

    queue form

--- src/Components/QueueForm.jsx
+++ src/Components/QueueForm.jsx
@@ -1,20 +1,45 @@
+import { useState } from "react"
+
+
 export default function QueueForm() {
+
+  const [name, setName] = useState("")
+  const [phone, setPhone] = useState("")
+  const [selection, setSelection] = useState("")
+
+  function submitName(e){
+    setName(e.target.value)
+  }
+  function submitPhone(e){
+    setPhone(e.target.value)
+  }
+  function submitSelection(e){
+    setSelection(e.target.value)
+  }
+
+  function submitForm(e) {
+    e.preventDefault(); // Prevents page reload
+    console.log(name,phone,selection)
+  }
+
   return (
     <div>
-      <form action="">
+      <form action="" onClick={submitForm}>
         <label htmlFor="">Name</label>
-        <input type="text" />
+        <input type="text" onChange={submitName}/>
         
         <label htmlFor="">Phone Number</label>
-        <input type="text" />
+        <input type="text" onChange={submitPhone}/>
 
-        <label htmlFor="">Selection</label>
-        <select name="" id="">
-          <option value="">Sakit gigi</option>
-          <option value="">doctor apoiment</option>
-          <option value="">checkup</option>
-        </select>
-        <button>Submit</button>
+        <label for="appointment">Selection:</label>
+          <select name="appointment" id="appointment" onChange={submitSelection}>
+            <option value="" disabled selected>Select an option</option>
+            <option value="Emergency">Emergency</option>
+            <option value="Appointment">Appointment</option>
+            <option value="Checkup">Checkup</option>
+          </select>
+
+        <button type="submit">Submit</button>
       </form>
     </div>
   )
